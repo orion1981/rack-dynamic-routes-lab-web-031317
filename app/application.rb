dbf9d0732108e4154ed6f562b2b695933c693450
github username <github email address>
2017-04-03 16:40:18 -0400

    Done.

---
+++ app/application.rb
@@ -0,0 +1,23 @@
+class Application
+
+  @@items = []
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+      cart_item = req.path.split("/items/").last
+      if item = @@items.find{|i| i.name == cart_item}
+        resp.write item.price
+      else
+        resp.write "Item not found"
+        resp.status = 400
+      end
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+    resp.finish
+  end
+end
